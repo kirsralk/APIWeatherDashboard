85d84ff60edd9a16a2ff256a92f102b43670c543
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-20 15:24:58 -0600

    api query functional for 3 endpoints, UV index is going to require separate call

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
 // Here we are building the URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=" + searchInput + "&appid=" + APIKey;
+  "q=" + "Seattle" + "&appid=" + APIKey;
 
 // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
@@ -18,15 +18,16 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
 
         // Log the resulting object
         console.log(response);
+        
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;        
 
         // Transfer content to HTML
         $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
+        $("#tempDiv").text("Temperature: " + tempF);
+        $("#windDiv").text("Wind Speed: " + response.wind.speed);
+        $("#humDiv").text("Humidity: " + response.main.humidity);
+      
         // add temp content to html
         $(".temp").text("Temperature (K) " + response.main.temp);
         $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
@@ -36,3 +37,33 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
       });
+
+// // Separate API call required for UV index
+// var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?" +
+//   "q=" + "Seattle" + "&appid=" + APIKey;
+
+//       $.ajax({
+//         url: queryURL2,
+//         method: "GET"
+//       })
+//         // We store all of the retrieved data inside of an object called "response2"
+//         .then(function(response2) {
+  
+//           // Log the queryURL
+//           console.log(queryURL2);
+  
+//           // Log the resulting object
+//           console.log(response2);
+
+//           // Transfer content to HTML
+//           $("#humDiv").text("Humidity: " + response.main.humidity);
+        
+//           // add temp content to html
+//           $(".temp").text("Temperature (K) " + response.main.temp);
+//           $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+  
+//           // Log the data in the console as well
+//           console.log("Wind Speed: " + response.wind.speed);
+//           console.log("Humidity: " + response.main.humidity);
+//           console.log("Temperature (F): " + tempF);
+//         });
